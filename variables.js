90523b11262e3d95e8419fecda1d8750d1b95475
sarang3232 <72816187+sarang3232@users.noreply.github.com>
2023-05-31 23:34:35 -0500

    Update variables.js

--- variables.js
+++ variables.js
@@ -5,7 +5,7 @@ function actualizarDatos() {
     // ******* INDICADORES DEL TABLERO L8 *****************
       // MODIFICA LOS VALORES SEGUN EL DIA
     
-      Eficienciadeproduccion = '0%';
+      Eficienciadeproduccion = '90%';
       Rendimientojarabe=       '99.7%'; 
       Desperfectos=            '1.7%';
       CumplimientoLimpieza=    '100%'; 
