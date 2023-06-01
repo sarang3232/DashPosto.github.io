3ad95639e493d6293f420103509faa43fb28b01f
sarang3232 <72816187+sarang3232@users.noreply.github.com>
2023-06-01 08:26:42 -0500

    Actualizar variables1.js

--- variables1.js
+++ variables1.js
@@ -6,69 +6,69 @@ function actualizarDatos() {
       // MODIFICA LOS VALORES SEGUN EL DIA
     
       Eficienciadeproduccion = '72%';
-      Rendimientojarabe=       '99.7%'; 
+      Rendimientojarabe=       '100%'; 
       Desperfectos=            '1.7%';
       CumplimientoLimpieza=    '100%'; 
     
     //******* MEDICCION HORA A HORA *********************
     //           6-7am   7-8am    8-9am   9-10am   10-11am   11-12pm   12-13pm    13-14pm
-    Teorica= [  2500,    5000,   7501,   10001,    12501,    15501,     17501,      20002];
+    Teorica= [  3833,6355,8897,11439,13981,16523,19065,21507];
     
     //           6-7am   7-8am    8-9am   9-10am   10-11am   11-12pm   12-13pm    13-14pm
-    Real= [      2670,   5293,    7920,   7920,    9501,     10690,    12307,     14405];
+    Real= [  1250,0,0,0,0,0,0,0];
     
     
     //*********** EFICIENCIA HORA ***********************
     
     //                         Porcentaje hora 6-7am
-    Hora67.setAttribute('style', 'width: 75%;'); 
-    Hora67l.innerHTML =                  '75%';
+    Hora67.setAttribute('style', 'width: 33%;'); 
+    Hora67l.innerHTML =                  '33%';
     
     //                         Porcentaje hora 7-8am
-    Hora78.setAttribute('style', 'width: 90%;'); 
-    Hora78l.innerHTML =                  '90%';
+    Hora78.setAttribute('style', 'width: 0%;'); 
+    Hora78l.innerHTML =                  '0%';
     
     //                         Porcentaje hora 8-9am
-    Hora89.setAttribute('style', 'width: 75%;'); 
-    Hora89l.innerHTML =                  '75%';
+    Hora89.setAttribute('style', 'width: 0%;'); 
+    Hora89l.innerHTML =                  '0%';
     
     //                         Porcentaje hora 9-10am
-    Hora910.setAttribute('style', 'width: 75%;'); 
-    Hora910l.innerHTML =                  '75%';
+    Hora910.setAttribute('style', 'width: 0%;'); 
+    Hora910l.innerHTML =                  '0%';
     
     //                         Porcentaje hora 10-11am
-    Hora1011.setAttribute('style', 'width: 75%;'); 
-    Hora1011l.innerHTML =                  '75%';
+    Hora1011.setAttribute('style', 'width: 0%;'); 
+    Hora1011l.innerHTML =                  '0%';
     
     //                         Porcentaje hora 11-12pm
-    Hora1112.setAttribute('style', 'width: 75%;'); 
-    Hora1112l.innerHTML =                  '75%';
+    Hora1112.setAttribute('style', 'width: 0%;'); 
+    Hora1112l.innerHTML =                  '0%';
     
     //                         Porcentaje hora 12-13pm
-    Hora1213.setAttribute('style', 'width: 75%;'); 
-    Hora1213l.innerHTML =                  '75%';
+    Hora1213.setAttribute('style', 'width: 0%;'); 
+    Hora1213l.innerHTML =                  '0%';
     
     //                         Porcentaje hora 13-14am
-    Hora1314.setAttribute('style', 'width: 75%;'); 
-    Hora1314l.innerHTML =                  '75%';
+    Hora1314.setAttribute('style', 'width: 0%;'); 
+    Hora1314l.innerHTML =                  '0%';
     
     
     
     //***********Tiempo perdido y acumulado************
     
     //             6-7am   7-8am    8-9am   9-10am   10-11am   11-12pm   12-13pm    13-14pm         
-    TiempoPerdido=['0',      '1',     '1',   '60',    '24',     '33',     '24',      '12'];
+    TiempoPerdido=['60'];
     
     
     //             6-7am   7-8am    8-9am   9-10am   10-11am   11-12pm   12-13pm    13-14pm         
-    TiempoAcumulado=['0',    '1',     '2',    '62',    '86',     '119',   '143',     '143'];
+    TiempoAcumulado=['60'];
     
     
     
     //*************Pon las observaciones de cada intervalo de hora************ 
     
     //                                   Observacion de 6-7 am
-    Observacion67.innerHTML =                'Nuevo texto'; 
+    Observacion67.innerHTML =                'Botella defectuosa perforada, inventario paletizadora'; 
     
     //                                   Observacion de 7-8 am
     Observacion78.innerHTML =                'Nuevo texto'; 
@@ -95,30 +95,30 @@ function actualizarDatos() {
     
     //*****************Programa de produccion************* 
     //********************************Pon el sabor a producir 1*/
-    programa1.innerHTML =                'Hit ';
+    programa1.innerHTML =                'Colombiana cero 1000ml';
     
     //********************************Pon el sabor a producir 2 */
-    programa2.innerHTML =                'Hit ';
+    programa2.innerHTML =                'Kola Postobón 1000ml ';
     
     //********************************Pon el sabor a producir 3 */
-    programa3.innerHTML =                'Hit ';
+    programa3.innerHTML =                'Agua gas 600 ml ';
     
     
     //********************************Pon la cantidad de cajas y unidades 1*/
-    caja1.innerHTML =                'Cajas #  / Unidades # ';
+    caja1.innerHTML =                'Cajas 5000 / Unidades 60000 ';
     
     //********************************Pon la cantidad de cajas y unidades 2 */
-    caja2.innerHTML =                'Cajas #  / Unidades # ';
+    caja2.innerHTML =                'Cajas 5000  / Unidades 60000 ';
     
     //********************************Pon la cantidad de cajas y unidades 3 */
-    caja3.innerHTML =                'Cajas #  / Unidades # ';
+    caja3.innerHTML =                'Cajas 16041  / Unidades 385000 ';
     
     //*********************Marca si ya se termino la produccion del sabor********
     //********************Primer sabor**************************************** */
     marcar1.checked = false; // Poner false, si no se ha termino, o true si ya termino
     
     //********************Segundo sabor**************************************** */
-    marcar2.checked = true; // Poner false, si no se ha termino, o true si ya termino
+    marcar2.checked = false; // Poner false, si no se ha termino, o true si ya termino
     
     //********************Tercer sabor**************************************** */
     marcar3.checked = false; // Poner false, si no se ha termino, o true si ya termino
