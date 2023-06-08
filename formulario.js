b1a02f5999394b63645aaf2696e9be43588f61a6
sarang3232 <72816187+sarang3232@users.noreply.github.com>
2023-06-07 20:55:09 -0500

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -2,46 +2,13 @@
 
 // ***************************************************
 // ******** NO TOCAR *****
-var Eficienciadeproduccion = '72%';
-var Rendimientojarabe='99.7%'; 
-var Desperfectos='1.7%';
-var CumplimientoLimpieza='100%';
-var Teorica= [2500, 5000, 7501, 10001, 12501, 15501,17501,20002];
-var Real = [2670, 5293, 7920, 7920, 9501, 10690,12307,14405];
-var Teorica1= [2500, 5000, 7501, 10001, 12501, 15501,17501,20002];
-var Real1 = [2670, 5293, 7920, 7920, 9501, 10690,12307,14405];
 var Teorica2= [2500, 5000, 7501, 10001, 12501, 15501,17501,20002];
 var Real2 = [2670, 5293, 7920, 7920, 9501, 10690,12307,14405];
-var TiempoPerdido=['0','1','1','60','24','33','24','24'];
-var TiempoAcumulado= ['0','1','2','62','86','119','143','143'];
 var TiempoPerdido1=['0','1','1','60','24','33','24','24'];
 var TiempoAcumulado1= ['0','1','2','62','86','119','143','143'];
 var TiempoPerdido2=['0','1','1','60','24','33','24','24'];
 var TiempoAcumulado2= ['0','1','2','62','86','119','143','143'];
-var Hora67= document.getElementById('progressBar1');
-var Hora67l = document.getElementById('percent1');
-var Hora78= document.getElementById('progressBar2');
-var Hora78l = document.getElementById('percent2'); 
-var Hora89= document.getElementById('progressBar3');
-var Hora89l = document.getElementById('percent3'); 
-var Hora910= document.getElementById('progressBar4');
-var Hora910l = document.getElementById('percent4'); 
-var Hora1011= document.getElementById('progressBar5');
-var Hora1011l = document.getElementById('percent5'); 
-var Hora1112= document.getElementById('progressBar6');
-var Hora1112l = document.getElementById('percent6'); 
-var Hora1213= document.getElementById('progressBar7');
-var Hora1213l = document.getElementById('percent7'); 
-var Hora1314= document.getElementById('progressBar8');
-var Hora1314l = document.getElementById('percent8');
-var Observacion67 = document.querySelector('.mb-0.my-label1');
-var Observacion78 = document.querySelector('.mb-0.my-label2');
-var Observacion89 = document.querySelector('.mb-0.my-label3');
-var Observacion910 = document.querySelector('.mb-0.my-label4');
-var Observacion1011 = document.querySelector('.mb-0.my-label5');
-var Observacion1112 = document.querySelector('.mb-0.my-label6');
-var Observacion1213 = document.querySelector('.mb-0.my-label7');
-var Observacion1314 = document.querySelector('.mb-0.my-label8');
+
 var programa1 = document.querySelector('.mb-0.my-pro1');
 var programa2 = document.querySelector('.mb-0.my-pro2');
 var programa3 = document.querySelector('.mb-0.my-pro3');
