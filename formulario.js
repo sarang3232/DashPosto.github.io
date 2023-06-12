61460b02690da8f68a3c82e021d4f7cf8cb88aff
sarang3232 <72816187+sarang3232@users.noreply.github.com>
2023-06-11 22:31:44 -0500

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,28 @@
+// formulario.js
+
+// ***************************************************
+// ******** NO TOCAR *****
+
+
+
+
+
+var TiempoPerdido1=['0','1','1','60','24','33','24','24'];
+var TiempoAcumulado1= ['0','1','2','62','86','119','143','143'];
+var TiempoPerdido2=['0','1','1','60','24','33','24','24'];
+var TiempoAcumulado2= ['0','1','2','62','86','119','143','143'];
+
+
+
+
+
+var programa3 = document.querySelector('.mb-0.my-pro3');
+
+
+var caja3 = document.querySelector('.text-muted.my-input3');
+var marcar1 = document.getElementById('customCheckBox1');
+var marcar2 = document.getElementById('customCheckBox2');
+var marcar3 = document.getElementById('customCheckBox3');
+
+
+
